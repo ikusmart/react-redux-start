2e73d94e261bef6bc4309fcea3cd3b716061cdb5
Ivan Kusmartsev <kusmartsev.ivan@gmail.com>
2018-04-16 23:13:03 +0300

    Added 4 types rendering of components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,129 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import ReactDOM from "react-dom"
 import './App.css';
+import data from './data.json';
+
+function findElement(arr, propName, propValue) {
+  for (var i=0; i < arr.length; i++)
+    if (arr[i][propName] == propValue)
+      return arr[i];
+
+  // will return undefined if not found; you could return a default instead
+}
+
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    
+    this.handleItemSelected = this.handleItemSelected.bind(this);
+	  this.state = {films: [], currentId: null};
+  }
+  componentWillMount(){
+    this.state.films.push(data.data);
+    this.setState({films: data.data});
+  }
+
+  handleItemSelected(id) {
+    this.setState({ currentId: id });
+}
+
   render() {
+    var selectedFilm = this.state.films.currentId ? findElement(this.state.films, "id", this.state.films.currentId) : null;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <Details {...selectedFilm}/>
+          {/*  */}
+          <List films={this.state.films} onItemSelected={this.handleItemSelected}/>
       </div>
     );
   }
 }
 
 export default App;
+
+class Details extends React.PureComponent {
+  constructor(props){
+    super(props);
+    
+	  this.state = {
+      id: this.props.id,
+      title: this.props.title, 
+      vote_average: this.props.vote_average, 
+      poster_path: this.props.poster_path,
+      overview: this.props.overview, 
+      budget: this.props.budget,
+    };
+    
+  }
+
+  
+
+  render() {
+
+    var filmDetails = this.props.id 
+                                  ? <div>
+                                      <img className="list-item-poster" src={this.state.poster_path} />
+                                      <h3>{this.state.title}}</h3>
+                                      <h2>{this.state.overview}}</h2>
+                                      <p>Vote - {this.state.vote_average}</p>
+                                    </div>
+                                  : <h1>No item selected</h1>;
+
+    return (
+      <div className="details">
+        {filmDetails}
+      </div>
+    );
+  }
+}
+
+
+
+class ListItem extends React.Component {
+  constructor(props){
+		super(props);
+	  this.state = {id: this.props.id, title: this.props.title, vote_average: this.props.vote_average, poster_path: this.props.poster_path};
+	}
+
+  render() {
+    return (
+      <div className="list-item"> 
+        <img className="list-item-poster" src={this.state.poster_path} />
+        <h3>{this.state.title}}</h3>
+        <p>Vote - {this.state.vote_average}</p>
+      </div>
+    );
+  }
+}
+
+class List extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {currentId: this.props.currentId, films: this.props.films};
+  }
+
+  render() {
+      return (
+        <div className="list-films">
+            {this.state.films.map(film=><ListItem
+                      key={film.id}
+                      id={film.id}
+                      title={film.title}
+                      vote_average={film.vote_average}
+                      poster_path = {film.poster_path}
+                      // onTaskDelete={handleTaskDel} 
+                      />)}
+        </div>
+      )
+  }
+}
+
+
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -2,7 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import logo from './Netflix.png'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function AppLogo(props) {
+return (<div className="header-logo"><img src={logo}/> <p>Logo created by function</p> </div>)
+}
+
+
+
+const header =
+  React.createElement('div', {className:"Header fix-position"},
+    React.createElement('div', {className:"header-title"},
+        React.createElement('h1', {}, 'Title - This title created by "createElement" ')
+    ),
+    <AppLogo />
+    // React.createElement('img', {src:logo, className:"header-logo"})
+  );
+
+  var app = React.createElement("div",null,header,<App />);
+
+
+ReactDOM.render(app, document.getElementById('root'));
 registerServiceWorker();
